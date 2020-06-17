3f50c8236f30fcdf6ec046049ace42e583172a57
Thomas Christerus <tchristerus@outlook.com>
2020-06-17 16:11:30 +0200

    Add: Command pattern
    Co-authored-by: @LennartPNL<lp.lennartpikijn@gmail.com>

--- View Models/CoffeeMachineModeViewModel.cs
+++ View Models/CoffeeMachineModeViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace Amnista.View_Models
+﻿using System.Windows;
+using System.Windows.Input;
+using Amnista.Generic;
+using Amnista.Models;
+
+namespace Amnista.View_Models
 {
-    class CoffeeMachineModeViewModel
+    class CoffeeMachineModeViewModel : ObservableObject
     {
+        private readonly Server _server;
+
+        public CoffeeMachineModeViewModel()
+        {
+            _server = new Server();
+        }
+
+        public ICommand StartServerCommand => new Commander(StartServer);
+
+        public void StartServer()
+        {
+            
+        }
     }
 }
